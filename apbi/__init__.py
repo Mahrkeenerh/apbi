303088290c75ee7ba71f5c34225d053690741a2f
Mahrkeenerh <samuelbuban@gmail.com>
2022-06-08 21:21:34 +0200

    Fix package importing

--- apbi/__init__.py
+++ apbi/__init__.py
@@ -1,7 +1,7 @@
 """APBI: asynchronous Python Bazos interface."""
 
 
-from models import sections
+from apbi.models import sections
 # from models import notice
 
 # from bazos import Bazos
--- apbi/models/__init__.py
+++ apbi/models/__init__.py
@@ -1,5 +1,5 @@
 """Models for the apbi library."""
 
 
-import sections
+from apbi.models import sections
 # from notice import Notice
